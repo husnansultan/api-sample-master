29f55789c9c8fc06e38d3d0cfe90371a3e19ce6d
husnansultan <husnan.sultan@academytrainee.com>
2019-01-16 16:25:27 +0000

    ss
    ss

---
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -0,0 +1,48 @@
+package com.qa.persistence.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.qa.business.service.AccountService;
+import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
+
+public class AccountMapRepository implements AccountRepository{
+
+	private HashMap<Integer, com.qa.persistence.domain.Account> accounts = new HashMap<>();
+	private int counter = 1;
+	private JSONUtil json = new JSONUtil();
+
+	public Account getmyAccount(int accountNumber) {
+		return accounts.get(accountNumber);
+
+	}
+
+	public int searchAccounts(String firstName) {
+
+		List<Account> newlist = (accounts.values().stream()
+				.filter(a -> a.getFirstName() == firstName)
+				.collect(Collectors.toList()));
+		return newlist.size();
+
+	}
+
+	@Override
+	public String getAllAccounts() {
+		
+		return json.getJSONForObject(accounts);
+	}
+
+	@Override
+	public String createAccount(String accout) {
+		Account anAccount = util.getObjectForJSON(accout, Account.class);
+		return null;
+	}
+
+	@Override
+	public String deleteAccount(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
